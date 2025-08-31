22149e8bb31e9442e681847d8f45544f77853d8b
SuyogPawara <152012409+SuyogPawara@users.noreply.github.com>
2025-08-31 11:26:31 +0530

    fix: set correct base for vercel

--- vite.config.ts
+++ vite.config.ts
@@ -28,7 +28,7 @@ export default defineConfig({
     outDir: path.resolve(import.meta.dirname, "dist"),
     emptyOutDir: true,
   },
-  base: "/PawaraPortfolio/",
+  base: "/",
   server: {
     fs: {
       strict: true,
